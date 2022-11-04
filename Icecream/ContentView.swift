62da84407c2b37d42d2e071746747169abad5582
Song Jiyeon <jiyeon.song@riiid.co>
2022-11-04 15:29:25 +0900

    :sparkles: Initial commit

---
+++ Icecream/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Icecream
+//
+//  Created by 송지연 on 2022/11/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Icecream/IcecreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  IcecreamApp.swift
+//  Icecream
+//
+//  Created by 송지연 on 2022/11/04.
+//
+
+import SwiftUI
+
+@main
+struct IcecreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
